a50940ef9c8f99b6a4fe8c033da14cc195cb2dc2
KarthikBashatwar5 <karthikbashatwar7799@gmail.com>
2026-09-07 15:43:31 +0530

    Add files via upload
    create a class with multiple objects

---
+++ classmul.py
@@ -0,0 +1,13 @@
+class demo
+def addition (self,a,b):
+    print(a+b)
+def subtraction(self,a,b):
+        print (a-b)
+def multiplication (self,a,b):
+              print (a*b)
+              d1=demo()
+              d1.addition(1,3)
+              d2=demo()
+              d2.subtraction(5,1)
+              d3=demo()
+              d3.multiplication(2,2)
