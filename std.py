dea9372e94e09c7faa2f9cbb0ef6673d632e1486
KarthikBashatwar5 <karthikbashatwar7799@gmail.com>
2026-09-07 15:42:46 +0530

    Add files via upload

---
+++ std.py
@@ -0,0 +1,13 @@
+class student:
+    name = "KPRIT"
+    age = 15
+    def read():
+        print("reading")
+        def write():
+            print("writing")
+            def open():
+                print("opening")
+                s1=student()
+                s1.read()
+                s1.write()
+                s1.open()
