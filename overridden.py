6ef0c1071f5586d5f5b9eb49be9c98d072f6b793
KarthikBashatwar5 <karthikbashatwar7799@gmail.com>
2026-09-07 15:40:58 +0530

    Add files via upload
    create inheritance with method overridden

---
+++ overridden.py
@@ -0,0 +1,9 @@
+class animal 
+def cat(self)
+    print("animal eat food")
+    def dog (animal)
+        def speak (self)
+            print ("dog bark"):
+            d1=dog()
+            d1.eat()
+            d1.speak()
